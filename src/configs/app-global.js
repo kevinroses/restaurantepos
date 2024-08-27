36f2047af3f59486cefa2682ca8d89b8caf445f6
kentur <kevinxrturop@gmail.com>
2024-08-27 11:08:51 -0500

    Update app-global.js

--- src/configs/app-global.js
+++ src/configs/app-global.js
@@ -1,7 +1,7 @@
 export const PROJECT_NAME = 'Foodyman marketplace';
 export const BASE_URL =
   process.env.REACT_APP_BASE_URL || 'https://api.foodyman.org';
-export const WEBSITE_URL = 'https://foodyman.org';
+export const WEBSITE_URL = 'https://restaurantepos.camanchacapp.cl';
 export const api_url = BASE_URL + '/api/v1/';
 export const api_url_admin = BASE_URL + '/api/v1/dashboard/admin/';
 export const api_url_admin_dashboard = BASE_URL + '/api/v1/dashboard/';
